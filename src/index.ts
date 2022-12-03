8c7802accadb8b9b4fa67c9eef12fb3d28c94544
cullymw <61572113+cullymw@users.noreply.github.com>
2022-12-02 23:42:58 -0600

    Create menus endpoint (#1)
    Add cors and menus endpoint

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,24 @@
+import { IMenuItem } from './MenuItem';
 
 require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser')
 const app = express();
+var cors = require('cors');
+app.use(cors());
 var jsonParser = bodyParser.json()
 
 const port = process.env.PORT || 3000;
+const menus: { breakfast: IMenuItem[], lunch: IMenuItem[], dinner: IMenuItem[] } = require('../data/menus.json');
 
 app.get('/', (req: any, res: any) => {
     res.status(200);
-    return res.json('Server is working'); 
+    return res.json('Server is working');
+});
+
+app.get('/menus', (req: any, res: any) => {
+    res.status(200);
+    return res.json(menus);
 });
 
 app.listen(port, () => {
